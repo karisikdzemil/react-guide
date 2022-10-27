c1a124f61c7a8dbc5c96a33bde207d1eb7dff054
karisikdzemil <karisikdzemil@gmail.com>
2022-10-27 17:11:18 +0200

    ----

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+// import Expenses from "./Expenses";
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
